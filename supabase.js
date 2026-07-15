55f1ba1725a6de9aa93b8c55a119957e0a5b07c3
je4nc4rlos0903-creator <je4nc4rlos0903@gmail.com>
2026-07-14 21:02:26 -0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,26 @@
+// =====================================
+// CONEXÃO SUPABASE
+// =====================================
+
+
+// Dados públicos do Supabase
+
+const SUPABASE_URL =
+"https://oimzoelsxwrciuvwsgjp.supabase.co";
+
+
+const SUPABASE_KEY =
+"sb_publishable_zMtQpqWl6yIwvZPRi_W66A_FGG-gTZA";
+
+
+
+// Criar conexão
+
+const supabase =
+window.supabase.createClient(
+
+    SUPABASE_URL,
+
+    SUPABASE_KEY
+
+);
